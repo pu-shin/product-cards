8fa49de50a7c2cb130311b72257792c4f0f981ae
Vulpes <goldenfoxtheninja@gmail.com>
2022-10-06 23:37:57 +0400

    rename classes in form

--- src/App.vue
+++ src/App.vue
@@ -11,50 +11,53 @@
       </div>
     </header>
     <main class="mainpage">
-      <div class="content">
-        <div class="content__container">
-          <div class="content__form">
-            <form action="" class="form-control">
-              <label class="form-control__label" for="name"
-                >Наименование товара</label
-              >
-              <input
-                class="form-control__input"
-                type="text"
-                id="name"
-                required
-                placeholder="Введите наименование товара"
-              />
-              <label class="form-control__label" for="desc"
-                >Описание товара</label
-              >
-              <input
-                class="form-control__input"
-                type="text"
-                id="desc"
-                placeholder="Введите описание товара"
-              />
-              <label class="form-control__label" for="link"
-                >Ссылка на изображение товара</label
-              >
-              <input
-                class="form-control__input"
-                type="text"
-                id="link"
-                required
-                placeholder="Введите ссылку"
-              />
-              <label class="form-control__label" for="price">Цена товара</label>
-              <input
-                class="form-control__input"
-                type="text"
-                id="price"
-                required
-                placeholder="Введите цену"
-              />
-              <button class="button">Добавить товар</button>
-            </form>
-          </div>
+      <div class="product">
+        <div class="product__container">
+          <form class="product__form form-control" action="" @submit.prevent>
+            <label
+              class="form-control__label form-control__label_required"
+              for="name"
+              >Наименование товара</label
+            >
+            <input
+              class="form-control__item"
+              type="text"
+              id="name"
+              placeholder="Введите наименование товара"
+            />
+            <label class="form-control__label" for="desc"
+              >Описание товара</label
+            >
+            <textarea
+              class="form-control__item form-control__textarea"
+              type="text"
+              id="desc"
+              placeholder="Введите описание товара"
+            />
+            <label
+              class="form-control__label form-control__label_required"
+              for="link"
+              >Ссылка на изображение товара</label
+            >
+            <input
+              class="form-control__item"
+              type="text"
+              id="link"
+              placeholder="Введите ссылку"
+            />
+            <label
+              class="form-control__label form-control__label_required"
+              for="price"
+              >Цена товара</label
+            >
+            <input
+              class="form-control__item"
+              type="text"
+              id="price"
+              placeholder="Введите цену"
+            />
+            <button class="form-control__button">Добавить товар</button>
+          </form>
           <div class="content__cards"></div>
         </div>
       </div>
